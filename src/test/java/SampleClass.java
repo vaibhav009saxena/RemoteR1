11f8e1556a58868a9bf366330fa6b0635b7a3bd3
Vaibhav Saxena <VAibhav009Saxena@gmail.com>
2024-09-24 20:52:41 +0530

    2nd commit by x person

---
+++ src/test/java/SampleClass.java
@@ -0,0 +1,12 @@
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// This class was added by X person orX develpoer
+
+	}
+
+}
